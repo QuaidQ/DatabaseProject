98fba0285931e494d424935cab90afc5d1b3488d
Jay Allen <jayrallen2@gmail.com>
2017-11-28 01:07:28 -0600

    stocked

---
+++ AmzonGoPhPScripts/stocked.php
@@ -0,0 +1,16 @@
+<?php
+   include('eSession.php');
+   $space = " ";
+?>
+<html>
+
+   <head>
+      <title>Welcome </title>
+   </head>
+
+   <body>
+      <h1>YOU'VE SUBMITTED YOUR STOCK REQUEST</h1>
+      <li><a href="<?php echo "logout.php";?>"> Exit Store </a></li>
+   </body>
+
+</html>
