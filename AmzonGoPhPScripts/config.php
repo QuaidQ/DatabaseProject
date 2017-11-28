5092022efb80fa669198a10d087cdf25af6b1280
Jay Allen <jayrallen2@gmail.com>
2017-11-28 01:05:24 -0600

    stuff

--- AmzonGoPhPScripts/config.php
+++ AmzonGoPhPScripts/config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Whispican75');
+define('DB_PASSWORD', 'fujiia47');
 define('DB_NAME', 'AmazonGO');
 
 /* Attempt to connect to MySQL database */
